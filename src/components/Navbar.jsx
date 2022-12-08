6bd92ea02e4a46aaafd72383128b1195a58be958
nilsondream <nilsonroalimachi@gmail.com>
2022-12-08 10:08:34 -0500

    responsive mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { Cpu, HandbagSimple, Headset, Keyboard, List, /*MagnifyingGlass,*/ Mouse, User } from 'phosphor-react'
+import { Cpu, HandbagSimple, Headset, House, Keyboard, List, Monitor, MagnifyingGlass, Mouse, User, X } from 'phosphor-react'
 
 const linksNavbar = [
-    { name: 'Audífonos', to: '/headsets', icon: <Headset size={20} /> },
-    { name: 'Mouses', to: '/mouses', icon: <Mouse size={20} /> },
-    { name: 'Teclados', to: '/keyboards', icon: <Keyboard size={20} /> },
-    { name: 'CPU', to: '/cpus', icon: <Cpu size={20} /> },
+    { name: 'Inicio', to: '/', icon: <House size={30} /> },
+    { name: 'Audífonos', to: '/headsets', icon: <Headset size={30} /> },
+    { name: 'Mouses', to: '/mouses', icon: <Mouse size={30} /> },
+    { name: 'Teclados', to: '/keyboards', icon: <Keyboard size={30} /> },
+    { name: 'Monitores', to: '/monitors', icon: <Monitor size={30} /> },
+    { name: 'CPU', to: '/cpus', icon: <Cpu size={30} /> },
 ];
 
 const Navbar = () => {
+
+    // open sidebar
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
+
     return (
-        <nav>
-            <div className='nav-left'>
-                <div className='btn-menu'><List /></div>
-                <NavLink to={'/'} className='logo'>
-                    <h1>Gaming</h1>
-                    <span>store</span>
-                    <h2>GS</h2>
-                </NavLink>
-                <div className='links'>
-                    {linksNavbar.map((item) => (
-                        <NavLink key={item.name} className={({ isActive }) => (isActive ? 'active' : '')} to={item.to}>
-                            {item.name}
-                        </NavLink>
-                    ))}
+        <>
+            <nav>
+                <div className='nav-left'>
+                    <div className='btn-menu' onClick={showSidebar}><List /></div>
+                    <NavLink to={'/'} className='logo'>
+                        <h1>Gaming</h1>
+                        <span>store</span>
+                    </NavLink>
+                    <div className='links'>
+                        {linksNavbar.map((item) => (
+                            <NavLink key={item.name} className={({ isActive }) => (isActive ? 'active' : '')} to={item.to}>
+                                {item.name}
+                            </NavLink>
+                        ))}
+                    </div>
                 </div>
-            </div>
-            {/*<div className='nav-center'>
+
+                <div className='nav-center'>Gaming Store</div>
+                {/*<div className='nav-center'>
                 <label htmlFor="">
                     <MagnifyingGlass />
                     <input type="text" placeholder='Buscar' />
                 </label>
                     </div>*/}
-            <div className='nav-right'>
-                <HandbagSimple />
-                <User />
+                <div className='nav-right'>
+                    <HandbagSimple />
+                    <User />
+                </div>
+            </nav>
+
+            <div className={sidebar ? 'sidebar-mobile sidebar-mobile-active' : 'sidebar-mobile'}>
+                <div className='btn-mobile-close' onClick={showSidebar}><X /></div>
+                <div className='sidebar-links'>
+                    {linksNavbar.map((item) => (
+                        <NavLink key={item.name} className={({ isActive }) => (isActive ? 'active' : '')} to={item.to} onClick={showSidebar}>
+                            {item.icon}{item.name}
+                        </NavLink>
+                    ))}
+                </div>
+                <label htmlFor="">
+                    <MagnifyingGlass size={30} />
+                    <input type="text" placeholder='Buscar producto' />
+                </label>
+
+                <div className='buttons'>
+                    <button>Iniciar</button>
+                    <button>Registrarse</button>
+                </div>
             </div>
-        </nav>
+        </>
     )
 }
 
